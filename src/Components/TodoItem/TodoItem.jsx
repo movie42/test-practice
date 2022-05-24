bd9cb987744cacbfa5731f8f6ee6178921104715
ko Hyunsu <movie3342@gmail.com>
2022-05-24 18:38:45 +0900

    통합 테스트

--- src/Components/TodoItem/TodoItem.jsx
+++ src/Components/TodoItem/TodoItem.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 const TodoItem = ({ todo, onToggle, onRemove }) => {
   const { id, text, done } = todo;
 
+  const toggle = useCallback(() => {
+    onToggle(id);
+  }, [id, onToggle]);
+  const remove = useCallback(() => {
+    onRemove(id);
+  }, [id, onRemove]);
+
   return (
     <li>
       <span
-        onClick={() => onToggle(id)}
+        onClick={toggle}
         style={{ textDecoration: done ? "line-through" : "none" }}
       >
         {text}
       </span>
-      <button onClick={() => onRemove(id)}>삭제</button>
+      <button onClick={remove}>삭제</button>
     </li>
   );
 };
 
-export default TodoItem;
+export default React.memo(TodoItem);
