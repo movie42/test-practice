679f9be978975b0e360b6fe822d27e76326ee625
ko Hyunsu <movie3342@gmail.com>
2022-05-24 16:33:07 +0900

    todo 초기화

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
-import React, { useCallback, useRef, useState } from "react";
-
-import TodoForm from "./Components/TodoForm/TodoForm";
-import TodoList from "./Components/TodoList/TodoList";
+import React from "react";
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-
-  const nextId = useRef(3);
-
-  const onInsert = useCallback((text) => {
-    setTodos((pre) => [...pre, { id: nextId.current, text, done: false }]);
-    nextId.current += 1;
-  });
-
-  const onToggle = useCallback((id) => {
-    setTodos((todos) =>
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo,
-      ),
-    );
-  }, []);
-
-  const onRemove = useCallback((id) => {
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
-  }, []);
-
-  return (
-    <>
-      <TodoForm onInsert={onInsert} data-testid="todo-form" />
-      <TodoList onRemove={onRemove} onToggle={onToggle} todos={todos} />
-    </>
-  );
+  return <div>App</div>;
 };
 
 export default App;
